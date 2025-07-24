f62f578ca71646eebe42f6a9230d09155b122ef1
Hirula Abesignha <hirulapinibinda01@gmail.com>
2025-07-24 09:20:56 +0530

    Update ml_detector.py

--- ml_detector.py
+++ ml_detector.py
@@ -1,14 +1,11 @@
-# ml_detector.py
 
 import joblib
 import os
 
-# Paths
 BASE_DIR = r"D:\phishing_email_detector"
 MODEL_FILE = os.path.join(BASE_DIR, "phishing_model.pkl")
 VECTORIZER_FILE = os.path.join(BASE_DIR, "vectorizer.pkl")
 
-# Load model and vectorizer
 try:
     clf = joblib.load(MODEL_FILE)
     vectorizer = joblib.load(VECTORIZER_FILE)
@@ -17,10 +14,9 @@ except FileNotFoundError:
     vectorizer = None
     print("❌ Model or vectorizer not found. Please train the model first using train_model.py.")
 
-# Predict function
 def predict_email(body_text):
     if clf is None or vectorizer is None:
         return None
     features = vectorizer.transform([body_text])
     prediction = clf.predict(features)[0]
-    return prediction  # 1 = phishing, 0 = safe
+    return prediction 
